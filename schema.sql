58fb678c208e48f6f70ca2c1a5e00f7c8c17aab6
viktor kan <viktorkan9@gmail.com>
2020-08-20 11:32:47 +0300

    --fix: add DROP TABLE IF EXIST

--- schema.sql
+++ schema.sql
@@ -3,6 +3,33 @@ CHARACTER SET utf8;
 
 USE taskforce;
 
+DROP TABLE IF EXISTS `categories`;
+
+DROP TABLE IF EXISTS `users`;
+
+DROP TABLE IF EXISTS `cities`;
+
+DROP TABLE IF EXISTS `files`;
+
+DROP TABLE IF EXISTS `profiles`;
+
+DROP TABLE IF EXISTS `profile_categories`;
+
+DROP TABLE IF EXISTS `profile_settings`;
+
+DROP TABLE IF EXISTS `tasks`;
+
+DROP TABLE IF EXISTS `profile_portfolios`;
+
+DROP TABLE IF EXISTS `task_files`;
+
+DROP TABLE IF EXISTS `task_response`;
+
+DROP TABLE IF EXISTS `task_message`;
+
+DROP TABLE IF EXISTS `task_feedbacks`;
+
+
 CREATE TABLE `categories` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) UNIQUE,

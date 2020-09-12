acbdd494de17908e4242ac4ab40f92b0eda73a52
htmlviktor <47639976+htmlviktor@users.noreply.github.com>
2020-09-12 14:55:57 +0300

    delete sql files

--- queries/categories.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO TASK_FORCE.categories(name,icon) VALUES 
-	("Переводы","translation"),
-	("Уборка","clean"),
-	("Переезды","cargo"),
-	("Компьютерная помощь","neo"),
-	("Ремонт квартирный","flat"),
-	("Ремонт техники","repair"),
-	("Красота","beauty"),
-	("Фото","photo");
